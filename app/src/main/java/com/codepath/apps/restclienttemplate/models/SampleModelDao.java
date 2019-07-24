fbe1ab4269457fa789176f507449a08710e1c7e5
Alexis <jeanwalneralexis@yahoo.com>
2019-07-24 16:57:58 -0500

    Initial commit

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/SampleModelDao.java
@@ -0,0 +1,29 @@
+package com.codepath.apps.restclienttemplate.models;
+
+//import android.arch.persistence.room.Dao;
+//import android.arch.persistence.room.Insert;
+//import android.arch.persistence.room.OnConflictStrategy;
+//import android.arch.persistence.room.Query;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface SampleModelDao {
+
+    // @Query annotation requires knowing SQL syntax
+    // See http://www.sqltutorial.org/
+
+    @Query("SELECT * FROM SampleModel WHERE id = :id")
+    SampleModel byId(long id);
+
+    @Query("SELECT * FROM SampleModel ORDER BY ID DESC LIMIT 300")
+    List<SampleModel> recentItems();
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertModel(SampleModel... sampleModels);
+}
